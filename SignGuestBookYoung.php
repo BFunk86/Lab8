e047875963fcf514678eee071ec4fd5adb13d722
Brandon Young <youngbd01@sunyjcc.edu>
2017-04-21 00:49:48 -0400

    Added SignGuestBookYoung.php to handle form submit from GuestBookYoung.html

--- SignGuestBookYoung.php
+++ SignGuestBookYoung.php
@@ -36,7 +36,6 @@
              * Author: Brandon Young
              */
 
-            $DBName = "GuestBook";
             // Check to see if information was entered into the Guest Book form
             if(empty($_POST['first_name']) || empty($_POST['last_name'])) {
                 echo "<p>You must enter your first and last name! Click your browser's Back button to " .
@@ -48,15 +47,55 @@
                     echo "<p>Unable to connect to the database server.</p>" .
                         "<p>Error code " . mysqli_errno() . ": " . mysqli_error() . "</p>";
                 } else {
+                    $DBName = "guestbook";
                     if( !@mysql_select_db($DBName, $DBConnect) ) {
                         $SQLstring = "CREATE DATABASE $DBName";
                         $QueryResult = @mysql_query($SQLstring, $DBConnect);
                         if($QueryResult === false) {
-                            echo "<p>Error Code " . mysql_errno($DBConnect)
-                        }
+                            echo "<p>Unable to execute the query.</p>" .
+                                "<p>Error Code " . mysql_errno($DBConnect) . ": " . mysql_error($DBConnect) . "</p>";
+                        } else {
+                            echo "<p>You are the first visitor!</p>";
+                        } // end if else
+                        mysql_select_db($DBName, $DBConnect);
                     }
                 }
+
+                // create the visitors table if it doesn't already exist
+                $TableName = "visitors";
+                $SQLstring = "SHOW TABLES LIKE '$TableName'";
+                $QueryResult = @mysql_query($SQLstring, $DBConnect);
+                if(mysql_num_rows($QueryResult) == 0) {
+                    $SQLstring = "CREATE TABLE $TableName 
+                (countID SMALLINT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+                last_name VARCHAR(40), first_name VARCHAR(40))";
+                    $QueryResult = @mysql_query($SQLstring, $DBConnect);
+                    if($QueryResult === false) {
+                        echo "<p>Unable to create the table.</p>"
+                            . "<p>Error code " . mysql_errno($DBConnect)
+                            . ": " . mysql_error($DBConnect) . "</p>";
+                    } // end if
+
+                    $LastName = stripslashes($_POST['last_name']);
+                    $FirstName = stripslashes($_POST['first_name']);
+                    $SQLstring = "INSERT INTO $TableName VALUES(NULL, '$LastName', '$FirstName')";
+                    $QueryResult = @mysql_query($SQLstring, $DBConnect);
+                    if($QueryResult === false) {
+                        echo "<p>Unable to create the table.</p>"
+                            . "<p>Error code " . mysql_errno($DBConnect)
+                            . ": " . mysql_error($DBConnect) . "</p>";
+                    } else {
+                        echo "<h1>Thank you for signing our guest book!</h1>";
+                    }
+                    mysql_close($DBConnect);
+
+                } // end if
+
             }
+
+
+
+
         ?>
     </div>
 </div>
